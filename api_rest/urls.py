f5088f039565d719ff87a1b800edc577421512c3
Lucas <prestoinker@gmail.com>
2025-02-05 15:43:22 -0300

    feat: views.py GET users/

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_users, name='get_all_users')
+    path('', views.get_users, name='get_all_users'),
+    path('user/<str:nick>', views.get_by_nickname),
+    path('data/', views.user_manager),
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+@api_view(['GET'])
+def get_users(request):
+
+    if request.method == 'GET':
+
+        users = User.objects.all()
+
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def get_by_nickname(request, nick):
+
+    try:
+        user = User.objects.get(pk=nick)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+
+    if request.method == 'GET':
+
+        try:
+            if request.GET['user']:
+                user_nickname = request.GET['user']
+                
+                try:
+                    user = User.objects.get(pk=user_nickname)
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
